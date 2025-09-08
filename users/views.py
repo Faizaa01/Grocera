39212b3f19c77b51aa656fc7aaf4e8774401edb2
Faizaa01 <faizaniha0062@gmail.com>
2025-09-08 12:17:42 +0600

    status updated

--- users/views.py
+++ users/views.py
@@ -177,7 +177,7 @@ def initiate_payment(request):
 def payment_success(request):
     order_id = request.data.get("tran_id").split('_')[1]
     order = Order.objects.get(id=order_id)
-    order.status = "Shipped"
+    order.status = "Processing"
     order.save()
     return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
 
