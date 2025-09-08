453887c7fb8a2f30777cf50f39bc24919f281004
Faizaa01 <faizaniha0062@gmail.com>
2025-09-08 12:11:17 +0600

    Payment Gateway related view added

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from rest_framework_nested import routers
 from product.views import ProductViewSet, ProductImageViewSet, CategoryViewSet, ReviewViewSet
 from order.views import WishlistViewSet, CartViewSet, CartItemViewSet, OrderViewset, HasOrderedProduct
-from users.views import DepositViewSet, DashboardView
+from users.views import DepositViewSet, DashboardView, initiate_payment, payment_success, payment_cancel, payment_fail
 
 
 router = routers.DefaultRouter()
@@ -34,4 +34,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('orders/has-ordered/<int:product_id>/', HasOrderedProduct.as_view()),
+    path("payment/initiate/", initiate_payment, name="initiate-payment"),
+    path("payment/success/", payment_success, name="payment-success"),
+    path("payment/fail/", payment_fail, name="payment-fail"),
+    path("payment/cancel/", payment_cancel, name="payment-cancel"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from .models import Deposit
-from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-
 from rest_framework import status
-from order.models import OrderItem
 from product.models import Product
+from sslcommerz_lib import SSLCOMMERZ
 from rest_framework.views import APIView
-from django.contrib.auth.models import Group
+from order.models import OrderItem, Order
 from rest_framework.response import Response
+from django.contrib.auth.models import Group
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics, permissions
+from django.shortcuts import HttpResponseRedirect
+from django.conf import settings as main_settings
 from rest_framework.permissions import IsAuthenticated
 from users.serializers import UserSerializer, DepositSerializer
 
@@ -126,3 +129,64 @@ class DepositViewSet(ModelViewSet):
     )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+
+
+
+
+@api_view(['POST'])
+def initiate_payment(request):
+    user = request.user
+    amount = request.data.get("amount")
+    order_id = request.data.get("orderId")
+    num_items = request.data.get("numItems")
+
+    settings = {'store_id': 'phima68a4a86fe8703',
+                'store_pass': 'phima68a4a86fe8703@ssl', 'issandbox': True}
+    sslcz = SSLCOMMERZ(settings)
+    post_body = {}
+    post_body['total_amount'] = amount
+    post_body['currency'] = "BDT"
+    post_body['tran_id'] = f"trx_{order_id}"
+    post_body['success_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/success/"
+    post_body['fail_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/fail/"
+    post_body['cancel_url'] = f"{main_settings.BACKEND_URL}/api/v1/payment/cancel/"
+    post_body['emi_option'] = 0
+    post_body['cus_name'] = f"{user.first_name} {user.last_name}"
+    post_body['cus_email'] = user.email
+    post_body['cus_phone'] = user.phone_number
+    post_body['cus_add1'] = user.address
+    post_body['cus_city'] = "Dhaka"
+    post_body['cus_country'] = "Bangladesh"
+    post_body['shipping_method'] = "NO"
+    post_body['multi_card_name'] = ""
+    post_body['num_of_item'] = num_items
+    post_body['product_name'] = "E-commerce Products"
+    post_body['product_category'] = "General"
+    post_body['product_profile'] = "general"
+
+    response = sslcz.createSession(post_body)
+
+    if response.get("status") == 'SUCCESS':
+        return Response({"payment_url": response['GatewayPageURL']})
+    return Response({"error": "Payment initiation failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['POST'])
+def payment_success(request):
+    order_id = request.data.get("tran_id").split('_')[1]
+    order = Order.objects.get(id=order_id)
+    order.status = "Ready To Ship"
+    order.save()
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+
+@api_view(['POST'])
+def payment_cancel(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+
+@api_view(['POST'])
+def payment_fail(request):
+    return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
